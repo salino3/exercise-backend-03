ce8f768c3a4332be9d6ace0120506156cde9940e
salino3 <flaviobcn012@gmail.com>
2023-07-29 13:00:36 +0200

    4º exercise done

--- index.js
+++ index.js
@@ -104,7 +104,7 @@ const winnerByYear = (arr, year) => {
   }, []);
 };
 
-console.log("----------------------")
+console.log("----------------------");
 console.log(winnerByYear(data3, 1998))
 console.log(winnerByYear(data3, 1999))
 console.log(winnerByYear(data3, 2000))
@@ -112,4 +112,121 @@ console.log(winnerByYear(data3, 2001))
 console.log(winnerByYear(data3, 2002))
 console.log(winnerByYear(data3, 2003))
 console.log(winnerByYear(data3, 2004))
+
+//
+//* Fouth exercise 
+
+
+const collection = [
+  {
+    id: "f0b6930c-331a-43e1-80db-e6c46ed552aa",
+    nationality: "Barbadians",
+    language: "English",
+    capital: "Belgrade",
+    national_sport: "taekwondo",
+    flag: "🇮🇩",
+  },
+  {
+    id: "3e690823-fc74-4376-999a-501e5f9ae4be",
+    nationality: "Congolese",
+    language: "German",
+    capital: "Kinshasa",
+    national_sport: "wrestling",
+    flag: "🇺🇾",
+  },
+  {
+    id: "9edd87d6-2f4f-4701-8ec4-272a361dbfe9",
+    nationality: "Libyans",
+    language: "Tagalog",
+    capital: "Jakarta",
+    national_sport: "buzkashi",
+    flag: "🇬🇼",
+  },
+  {
+    id: "9873a1ed-6dc5-4034-8214-1f428c8951bd",
+    nationality: "Guineans",
+    language: "Hakka",
+    capital: "Ankara",
+    national_sport: "gymnastics",
+    flag: "🇹🇷",
+  },
+  {
+    id: "4679c4a4-4e2e-4470-a900-2445dc1f4a1e",
+    nationality: "Ugandans",
+    language: "German",
+    capital: "Beijing",
+    national_sport: "dandi biyo",
+    flag: "🇮🇳",
+  },
+  {
+    id: "4274ad62-5089-4b8e-a002-b2c1c3c74926",
+    nationality: "Finns",
+    language: "Swedish",
+    capital: "Djibouti",
+    national_sport: "bull fighting",
+    flag: "🇭🇲",
+  },
+  {
+    id: "2bb25c20-7962-47b7-82d9-d62a9493308f",
+    nationality: "Poles",
+    language: "Swedish",
+    capital: "Beirut",
+    national_sport: "cricket",
+    flag: "🇰🇭",
+  },
+  {
+    id: "9b3e09da-7484-49f3-aed0-13ccc7e77fff",
+    nationality: "Guineans",
+    language: "Portuguese",
+    capital: "Guatemala City",
+    national_sport: "cricket",
+    flag: "🇩🇪",
+  },
+  {
+    id: "903fb062-647c-46f8-857f-c1eba0cbbc9b",
+    nationality: "Ivoirians",
+    language: "Nepali",
+    capital: "Juba",
+    national_sport: "cricket",
+    flag: "🇫🇮",
+  },
+  {
+    id: "3e690823-fc74-4376-999a-501e5f9ae4be",
+    nationality: "Hungarians",
+    language: "Russian",
+    capital: "Tarawa Atoll",
+    national_sport: "gymnastics",
+    flag: "🇲🇴",
+  },
+];
+
+const normalize = (arr) => {
+
+    let ids = arr.reduce((arr, obj) => {
+          if (obj.id) {
+            arr.push({id: obj.id});
+          };
+          return arr;
+    }, []);
+
+    let arr2 = arr.map((obj) => {
+        delete obj.id;
+        return obj;
+      });
+
+    let result = ids.map((idObj, index) => {
+        return {[idObj.id]: arr2[index]}
+    });
+
+     return result; 
+};
+
+console.log("----------------------");
+
+
+const result4 = normalize(collection);
+console.log(result4);
+
+
+
 
